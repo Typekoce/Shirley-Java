98e17c2528629c986327abac265fc59c03922070
Lee Majors <lee@majors.com>
2022-07-06 07:11:46 +0100

    Added menu model to page models

--- java-base/tests/build/HomeModel.java
+++ java-base/tests/build/HomeModel.java
@@ -1,14 +1,12 @@
 public class HomeModel extends AppModel{
 
-  public String[] menu = new String[3];
-
-  public String debug = "TRUE";
-
   HomeModel(AppData data){
 
-    this.data = data;
+    data.setData("page","home");
 
-    setMenu();
+    MenuModel menu = new MenuModel(data);
+
+    this.data = data;
 
     data.getDataArray("left","sidebar");
 
@@ -20,55 +18,5 @@ public class HomeModel extends AppModel{
 
   }
 
-  public AppProperties[] createProps() {
-
-    // Add number elements to matchDataProperties in AppData.java
-
-    AppProperties[] props = new AppProperties[data.AppPropertiesTotal];
-
-    props[0] = new AppProperties();
-
-    props[1] = new AppProperties();
-
-    props[2] = new AppProperties();
-
-    props[3] = new AppProperties();
-
-    props[0].set("icon","images/home/ipad_button_default.jpg");
-
-    props[1].set("rollover","images/home/default_rollover.jpg");
-
-    props[2].setNumber("imgWidth",232);
-
-    props[3].setNumber("imgHeight",70);
-
-    return props;
-  }
-
-  public void setMenu(){
-
-    data.setDataProperties("left","Nike",createProps());
-
-    data.setDataProperties("left","Football",createProps());
-
-    data.setDataProperties("left","Trail",createProps());
-
-    data.setDataProperties("left","Sprint",createProps());
-
-    data.setDataProperties("left","Rugby",createProps());
-
-    data.setDataProperties("left","Golf",createProps());
-
-    data.setDataProperties("left","Tennis",createProps());
-
-    data.setDataProperties("left","Squash",createProps());
-
-    data.setDataProperties("left","BMX",createProps());
-
-    data.setDataProperties("left","GYM",createProps());
-
-    data.setDataProperties("left","Hybrid",createProps());
-
-  }
 
 }
